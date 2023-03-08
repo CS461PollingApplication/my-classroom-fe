42c25b30dd24e39e8d0b741a279bc932adea51dc
Gaven Robertson <gaven.robertson@gmail.com>
2023-03-07 23:21:12 -0800

    added code for SingleCoursePage, changed temp JSON

---
+++ src/pages/SingleCoursePages.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route, Link, NavLink, useNavigate, useParams } from 'react-router-dom'
+
+//replcae this with the API call.
+const course = require('./data/courseData.json') 
+
+
+//This function is going to be used to display the single course page from when the
+//user selects the course.  
+function DisplayCoursePage(){
+//using useParams() here to grab the number in the course they select then finding the data
+//associated with that course.
+let { paramID } = useParams()
+
+return(
+    <div>
+        <h2>{course[paramID].course_name}</h2>
+        <p>{course[paramID].crn}</p>
+        <p>{course[paramID].course_instructor}</p>
+    </div>
+)
+
+}
+
+export default DisplayCoursePage
